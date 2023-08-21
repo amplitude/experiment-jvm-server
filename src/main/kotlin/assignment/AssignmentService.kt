8d3757b407aac9a89a7dfe09e3a8ab32db6eeb5f
Tim Yiu <tim.yiu@Tims-MacBook-Pro.local>
2023-08-21 14:35:27 -0700

    update dependencies

--- src/main/kotlin/assignment/AssignmentService.kt
+++ src/main/kotlin/assignment/AssignmentService.kt
@@ -1,50 +1,23 @@
 package com.amplitude.experiment.assignment
+
 import com.amplitude.Amplitude
 import com.amplitude.AmplitudeCallbacks
 import com.amplitude.Event
 import com.amplitude.experiment.evaluation.FLAG_TYPE_MUTUAL_EXCLUSION_GROUP
-import com.amplitude.experiment.util.wrapMetrics
 import org.json.JSONObject
 
 internal interface AssignmentService {
     fun track(assignment: Assignment)
 }
 
-class EventTrackingException(
-    event: Event,
-    status: Int,
-    message: String?
-) : Exception(
-    "Failed to track event to amplitude: event=${event.eventType}, status=$status, msg=$message"
-)
-
 internal class AmplitudeAssignmentService(
     private val amplitude: Amplitude,
     private val assignmentFilter: AssignmentFilter,
 ) : AssignmentService {
 
-    init {
-        amplitude.setCallbacks(object : AmplitudeCallbacks() {
-            override fun onLogEventServerResponse(event: Event?, status: Int, message: String?) {
-                if (event == null) return
-                if (status != 200) {
-                    metrics.onAssignmentEventFailure(EventTrackingException(event, status, message))
-                }
-            }
-        })
-    }
-
     override fun track(assignment: Assignment) {
-        metrics.onAssignment()
         if (assignmentFilter.shouldTrack(assignment)) {
-            wrapMetrics(
-                metric = metrics::onAssignmentEvent,
-                failure = metrics::onAssignmentEventFailure,
-            ) {
-                amplitude.logEvent(assignment.toAmplitudeEvent())
-            }
-        } else {
-            metrics.onAssignmentFilter()
+            amplitude.logEvent(assignment.toAmplitudeEvent())
         }
     }
 }
@@ -77,6 +50,7 @@ internal fun Assignment.toAmplitudeEvent(): Event {
         put("\$set", set)
         put("\$unset", unset)
     }
-    event.insertId = "${this.user.userId} ${this.user.deviceId} ${this.canonicalize().hashCode()} ${this.timestamp / DAY_MILLIS}"
+    event.insertId =
+        "${this.user.userId} ${this.user.deviceId} ${this.canonicalize().hashCode()} ${this.timestamp / DAY_MILLIS}"
     return event
 }
