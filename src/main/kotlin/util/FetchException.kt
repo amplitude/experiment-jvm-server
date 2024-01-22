b5eb1f2e4a3d088ea9aefaeee0f6775912c7323c
tyiuhc <tim.yiu@amplitude.com>
2024-01-22 11:31:20 -0800

    FetchException should extend IOException

--- src/main/kotlin/util/FetchException.kt
+++ src/main/kotlin/util/FetchException.kt
@@ -1,6 +1,8 @@
 package com.amplitude.experiment.util
 
+import okio.IOException
+
 class FetchException internal constructor(
     val statusCode: Int,
     message: String
-) : Exception(message)
+) : IOException(message)
