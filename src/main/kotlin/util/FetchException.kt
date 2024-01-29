93e82939ed3c8e0b9195d4eadd8c6bb4e40d5ae4
tyiuhc <tim.yiu@amplitude.com>
2024-01-29 14:42:29 -0800

    style: fix FetchException styling

--- src/main/kotlin/util/FetchException.kt
+++ src/main/kotlin/util/FetchException.kt
@@ -2,7 +2,7 @@ package com.amplitude.experiment.util
 
 import okio.IOException
 
-internal class FetchException (
+internal class FetchException(
     val statusCode: Int,
     message: String
 ) : IOException(message)
