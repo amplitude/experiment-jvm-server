d6d9684332066aaf5345646061651e67a7ea1247
Brian Giori <brian.giori@amplitude.com>
2024-09-20 14:44:11 -0700

    fix: fix assignment config to use batch when configured (#32)

--- src/main/kotlin/ServerZone.kt
+++ src/main/kotlin/ServerZone.kt
@@ -6,6 +6,8 @@ internal const val US_COHORT_SERVER_URL = "https://cohort-v2.lab.amplitude.com"
 internal const val EU_COHORT_SERVER_URL = "https://cohort-v2.lab.eu.amplitude.com"
 internal const val US_EVENT_SERVER_URL = "https://api2.amplitude.com/2/httpapi"
 internal const val EU_EVENT_SERVER_URL = "https://api.eu.amplitude.com/2/httpapi"
+internal const val US_BATCH_SERVER_URL = "https://api2.amplitude.com/batch"
+internal const val EU_BATCH_SERVER_URL = "https://api.eu.amplitude.com/batch"
 
 enum class ServerZone {
     US, EU
